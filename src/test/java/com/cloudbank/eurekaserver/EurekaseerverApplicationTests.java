54623895eec8c534e9e28fc8e4773220a9b433a3
Mark Satin <msatin2009@gmail.com>
2023-11-03 18:24:45 -0400

    first commit, setting up EuerekaServer

---
+++ src/test/java/com/cloudbank/eurekaserver/EurekaseerverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudbank.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaseerverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
